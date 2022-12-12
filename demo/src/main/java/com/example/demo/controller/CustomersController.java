bca0b04a427bb4d6d2000a5ed5b41451e171f9b3
Slobodan Barisic <slobodan.barisic@zooplus.com>
2022-12-12 10:31:00 +0100

    added some api responses and api operation descriptions

--- demo/src/main/java/com/example/demo/controller/CustomersController.java
+++ demo/src/main/java/com/example/demo/controller/CustomersController.java
@@ -1,29 +1,44 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/customers")
 public class CustomersController {
 
     private final List<String> CUSTOMERS = Arrays.asList("Jane", "Jack", "Donald");
 
-    @GetMapping("/all-customers")
+    @GetMapping("/all")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Fetches all customers from db.",
+            notes = "Dummy data used at the moment.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success"),
+    })
     public List<String> getAllCustomers(){
         return CUSTOMERS;
     }
 
     @GetMapping(value = "/customer/{name}")
     @ResponseStatus(HttpStatus.OK)
-    public String getCustomerByLastName(@PathVariable String name){
+    @ApiOperation(value = "Fetches specific customer by name from db.",
+            notes = "Dummy data used at the moment.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 404, message = "Not found")
+    })
+    public ResponseEntity<String> getCustomerByName(@PathVariable String name){
         if(CUSTOMERS.stream().map(String::toLowerCase).toList().contains(name.toLowerCase())){
-            return name;
+            return new ResponseEntity<>(name, HttpStatus.OK);
         }
-        return "We do not have that customer in our database!";
+        return new ResponseEntity<>("We do not have that customer in our database!", HttpStatus.NOT_FOUND);
     }
 }
--- demo/src/main/java/com/example/demo/controller/UsersController.java
+++ demo/src/main/java/com/example/demo/controller/UsersController.java
@@ -1,29 +1,44 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/users")
 public class UsersController {
 
     private final List<String> USERS = Arrays.asList("Aurora", "Antonija", "Manuela");
 
-    @GetMapping("/all-users")
+    @GetMapping("/all")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Fetches all users from db.",
+            notes = "Dummy data used at the moment.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success"),
+    })
     public List<String> getAllUsers(){
         return USERS;
     }
 
     @GetMapping(value = "/user/{name}")
     @ResponseStatus(HttpStatus.OK)
-    public String getUserByLastName(@PathVariable String name){
+    @ApiOperation(value = "Fetches specific user by name from db.",
+            notes = "Dummy data used at the moment.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 404, message = "Not found")
+    })
+    public ResponseEntity<String> getUserByName(@PathVariable String name){
         if(USERS.stream().map(String::toLowerCase).toList().contains(name.toLowerCase())){
-            return name;
+            return new ResponseEntity<>(name, HttpStatus.OK);
         }
-        return "We do not have that user in our database!";
+        return new ResponseEntity<>("We do not have that user in our database!", HttpStatus.NOT_FOUND);
     }
 }
