5c04573fb9a26739dfa0f3e37fcf6bd584e3ca66
Slobodan Barisic <slobodan.barisic@zooplus.com>
2022-12-16 15:20:31 +0100

    small change

--- exceptionhandling/src/main/java/com/example/config/WebMvcConfig.java
+++ exceptionhandling/src/main/java/com/example/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvc implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "swagger-ui.html");
