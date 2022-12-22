4ebb0fa82ffc9b63e27c2321607e1cf3bb0ed4af
Slobodan Barisic <slbarisic@gmail.com>
2022-12-22 12:00:51 +0100

    added check if we already have document with dame email in db

--- database/src/main/java/com/example/DatabaseInvestigationApplication.java
+++ database/src/main/java/com/example/DatabaseInvestigationApplication.java
@@ -4,6 +4,7 @@ import com.example.mongo.domain.enums.Gender;
 import com.example.mongo.domain.model.Address;
 import com.example.mongo.infrastructure.document.StudentDocument;
 import com.example.mongo.infrastructure.mongorepository.StudentMongoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +12,12 @@ import org.springframework.context.annotation.Bean;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
+@Slf4j
 public class DatabaseInvestigationApplication {
     public static void main(String[] args) {
         SpringApplication.run(DatabaseInvestigationApplication.class, args);
@@ -22,22 +26,31 @@ public class DatabaseInvestigationApplication {
     @Bean
     CommandLineRunner runner(StudentMongoRepository studentMongoRepository){
         return args -> {
-            StudentDocument studentDocument = StudentDocument.builder()
-                    .firstName("Manuela")
-                    .lastName("Barisic")
-                    .email("test.tes1@gmail.com")
-                    .gender(Gender.FEMALE)
-                    .address(Address.builder()
-                            .country("Germany")
-                            .city("Split")
-                            .postCode("21000")
-                            .build())
-                    .favoriteSubjects(List.of("Finances"))
-                    .totalSpentInBooks(BigDecimal.valueOf(15L))
-                    .created(LocalDateTime.now())
-                    .build();
+            StudentDocument document = buildStudentDocument();
+            Optional<StudentDocument> optionalDocFromDb = studentMongoRepository.findStudentDocumentByEmail(document.email());
 
-            studentMongoRepository.insert(studentDocument);
+            if(optionalDocFromDb.isPresent()){
+                log.warn("*****CANNOT INSERT DOCUMENT. THERE IS ONE IN DB WITH THE SAME EMAIL FIELD(indexed).");
+            }else{
+                studentMongoRepository.insert(document);
+            }
         };
+    }
+
+    private StudentDocument buildStudentDocument(){
+        return StudentDocument.builder()
+                .firstName("Manuela")
+                .lastName("Barisic")
+                .email("test.tes1@gmail.com")
+                .gender(Gender.FEMALE)
+                .address(Address.builder()
+                        .country("Germany")
+                        .city("Split")
+                        .postCode("21000")
+                        .build())
+                .favoriteSubjects(List.of("Finances"))
+                .totalSpentInBooks(BigDecimal.valueOf(15L))
+                .created(LocalDateTime.now())
+                .build();
     }
 }
--- database/src/main/java/com/example/mongo/infrastructure/mongorepository/StudentMongoRepository.java
+++ database/src/main/java/com/example/mongo/infrastructure/mongorepository/StudentMongoRepository.java
@@ -4,7 +4,10 @@ import com.example.mongo.infrastructure.document.StudentDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentMongoRepository extends MongoRepository<StudentDocument, Long> {
     List<StudentDocument> findStudentDocumentsByLastName(String lastName);
+
+    Optional<StudentDocument> findStudentDocumentByEmail(String email);
 }
