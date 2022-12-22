28f4bc186265e191acf0d47ea63cb6c73de9e301
Slobodan Barisic <slbarisic@gmail.com>
2022-12-22 01:23:51 +0100

    first commit for dealing with mongo db and spring boot

--- database/src/main/java/com/example/DatabaseInvestigationApplication.java
+++ database/src/main/java/com/example/DatabaseInvestigationApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class DatabaseInvestigationApplication {
     public static void main(String[] args) {
         SpringApplication.run(DatabaseInvestigationApplication.class, args);
