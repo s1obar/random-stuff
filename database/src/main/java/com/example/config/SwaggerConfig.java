8d16fa4bc8dd93dc11b10cfa83cc17ff2ffdde25
Slobodan Barisic <slbarisic@gmail.com>
2022-12-22 11:29:44 +0100

    added one more group, for mysql to swagger config

--- database/src/main/java/com/example/config/SwaggerConfig.java
+++ database/src/main/java/com/example/config/SwaggerConfig.java
@@ -27,6 +27,17 @@ public class SwaggerConfig {
     }
 
     @Bean
+    public Docket apiMySqlDb() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.mysql.controller"))
+                .paths(regex("/api/v1/.*"))
+                .build()
+                .groupName("mysql v1")
+                .apiInfo(apiMySqlInfo());
+    }
+
+    @Bean
     public Docket apiPostgresDb() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
@@ -42,6 +53,14 @@ public class SwaggerConfig {
                 .title("Mongodb Demo")
                 .version("1.0")
                 .description("Different CRUD services with MongoDB")
+                .build();
+    }
+
+    private ApiInfo apiMySqlInfo(){
+        return new ApiInfoBuilder()
+                .title("MySql Demo")
+                .version("1.0")
+                .description("Different CRUD services with MySql")
                 .build();
     }
 
