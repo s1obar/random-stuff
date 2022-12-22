0a8c44150616d8c9157af35c8a978a07f786fc17
Slobodan Barisic <slbarisic@gmail.com>
2022-12-22 02:32:01 +0100

    managed to create mongodb in docker container and run it in idea. managed to do simple query

--- database/src/main/java/com/example/DatabaseInvestigationApplication.java
+++ database/src/main/java/com/example/DatabaseInvestigationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
+//@EnableMongoRepositories
 public class DatabaseInvestigationApplication {
     public static void main(String[] args) {
         SpringApplication.run(DatabaseInvestigationApplication.class, args);
