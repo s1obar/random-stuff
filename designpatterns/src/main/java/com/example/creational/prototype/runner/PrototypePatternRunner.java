8f43f3d5f1537b89cb8c54ec3687908cd10ad5cc
Slobodan Barisic <slbarisic@gmail.com>
2023-01-16 11:29:05 +0100

    started with prototype design pattern

---
+++ designpatterns/src/main/java/com/example/creational/prototype/runner/PrototypePatternRunner.java
@@ -0,0 +1,12 @@
+package com.example.creational.prototype.runner;
+
+import lombok.Builder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Builder
+public class PrototypePatternRunner {
+    public void runPrototypeDesignPattern(){
+
+    }
+}
