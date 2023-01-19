9ab2bdb37556a379450cff5c793f028c29e74f7e
Slobodan Barisic <slbarisic@gmail.com>
2023-01-19 22:55:30 +0100

    added main class for aws

---
+++ aws/src/main/java/com/example/sqs/AwsInvestigationApplication.java
@@ -0,0 +1,11 @@
+package com.example.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsInvestigationApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AwsInvestigationApplication.class, args);
+    }
+}
