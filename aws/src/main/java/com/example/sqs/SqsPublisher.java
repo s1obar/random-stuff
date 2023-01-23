5043a7ac227a6f0974a281f36590e5534ed4fc76
Slobodan Barisic <slbarisic@gmail.com>
2023-01-23 21:15:21 +0100

    created simple publisher without recover method, did not test

--- aws/src/main/java/com/example/sqs/SqsPublisher.java
+++ aws/src/main/java/com/example/sqs/SqsPublisher.java
@@ -1,10 +1,32 @@
 package com.example.sqs;
 
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class SqsPublisher {
     @Value("${localstack.queue.name}")
     private String queueName;
+    private final AmazonSQSAsync localstackSqs;
 
-    
+    public void publish(String message){
+        final String messageId = UUID.randomUUID().toString();
+
+        final SendMessageRequest request =new SendMessageRequest()
+                .withQueueUrl(localstackSqs.getQueueUrl(queueName).getQueueUrl())
+                .withMessageDeduplicationId(messageId)
+                .withMessageGroupId(message)
+                .withMessageBody(message);
+
+        log.info("Publishing message {} to queue {}.", message, queueName);
+        localstackSqs.sendMessage(request);
+    }
 }
