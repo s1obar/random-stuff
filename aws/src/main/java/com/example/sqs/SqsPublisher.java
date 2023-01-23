bbdbf4aaa03ad93f3805948bf0ac23da6bf8af85
Slobodan Barisic <slbarisic@gmail.com>
2023-01-23 21:09:21 +0100

    added queue name to yml, using it with @Value annotation

--- aws/src/main/java/com/example/sqs/SqsPublisher.java
+++ aws/src/main/java/com/example/sqs/SqsPublisher.java
@@ -1,5 +1,10 @@
 package com.example.sqs;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SqsPublisher {
+    @Value("${localstack.queue.name}")
+    private String queueName;
+
     
 }
