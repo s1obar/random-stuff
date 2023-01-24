5a2c1ce0383866f3604568908e8bdb9a6915c845
Slobodan Barisic <slbarisic@gmail.com>
2023-01-24 07:47:53 +0100

    trying to tox swagger setup

--- aws/src/main/java/com/example/controller/AwsController.java
+++ aws/src/main/java/com/example/controller/AwsController.java
@@ -2,19 +2,24 @@ package com.example.controller;
 
 import com.example.sqs.SqsPublisher;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/aws")
+@RequestMapping("/api/v1/aws")
 @RequiredArgsConstructor
 public class AwsController {
     private final SqsPublisher sqsPublisher;
 
-    @PostMapping("publish-to-queue")
+    @PostMapping("/publish-to-queue")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation("Publishes a message to test queue on localstack.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success")
+    })
     public void publishToSqsQueue(@RequestParam String message){
         sqsPublisher.publish(message);
     }
