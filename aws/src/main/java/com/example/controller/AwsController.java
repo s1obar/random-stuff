f46d90d3dbd0e416e3f9b3c52d4c28d3cc7f66e6
Slobodan Barisic <slbarisic@gmail.com>
2023-01-24 07:18:18 +0100

    calling sqs publisher, added request param

--- aws/src/main/java/com/example/controller/AwsController.java
+++ aws/src/main/java/com/example/controller/AwsController.java
@@ -1,21 +1,22 @@
 package com.example.controller;
 
+import com.example.sqs.SqsPublisher;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/aws")
 @RequiredArgsConstructor
 public class AwsController {
+    private final SqsPublisher sqsPublisher;
+
     @PostMapping("publish-to-queue")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation("Publishes a message to test queue on localstack.")
-    public void publishToSqsQueue(){
-
+    public void publishToSqsQueue(@RequestParam String message){
+        sqsPublisher.publish(message);
     }
 }
