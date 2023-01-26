55a4c8fee50806bfe29c806f730843d9bac19b7b
Slobodan Barisic <slbarisic@gmail.com>
2023-01-26 22:40:35 +0100

    started with adapter design pattern

---
+++ designpatterns/src/main/java/com/example/structural/adapter/model/Voltage.java
@@ -0,0 +1,10 @@
+package com.example.structural.adapter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Voltage {
+    private int volts;
+}
