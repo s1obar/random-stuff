a3bb307daef9298c92b7cf6436e835ca15737e28
Slobodan Barisic <slbarisic@gmail.com>
2023-01-27 22:32:50 +0100

    added object adapter imlementation

--- designpatterns/src/main/java/com/example/structural/adapter/objectimpl/SocketObjectAdapterImpl.java
+++ designpatterns/src/main/java/com/example/structural/adapter/objectimpl/SocketObjectAdapterImpl.java
@@ -1,21 +1,30 @@
 package com.example.structural.adapter.objectimpl;
 
 import com.example.structural.adapter.SocketAdapter;
+import com.example.structural.adapter.model.Socket;
 import com.example.structural.adapter.model.Voltage;
 
 public class SocketObjectAdapterImpl implements SocketAdapter {
+    private final Socket socket = new Socket();
+
     @Override
     public Voltage get3Volts() {
-        return null;
+        Voltage volts120 = socket.getVoltage();
+        return convertVoltage(volts120, 40);
     }
 
     @Override
     public Voltage get12Volts() {
-        return null;
+        Voltage volts120 = socket.getVoltage();
+        return convertVoltage(volts120, 10);
     }
 
     @Override
     public Voltage get120Volts() {
-        return null;
+        return socket.getVoltage();
+    }
+
+    private Voltage convertVoltage(Voltage voltage, int nrOfTimesLessThenDefault){
+        return new Voltage(voltage.getVolts() / nrOfTimesLessThenDefault);
     }
 }
