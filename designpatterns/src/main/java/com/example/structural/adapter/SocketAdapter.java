082fd7a8df710716250a1b43394514efbcf260af
Slobodan Barisic <slbarisic@gmail.com>
2023-01-27 22:22:22 +0100

    added socket interface

--- designpatterns/src/main/java/com/example/structural/adapter/SocketAdapter.java
+++ designpatterns/src/main/java/com/example/structural/adapter/SocketAdapter.java
@@ -1,4 +1,9 @@
 package com.example.structural.adapter;
 
+import com.example.structural.adapter.model.Voltage;
+
 public interface SocketAdapter {
+    public Voltage get3Volts();
+    public Voltage get12Volts();
+    public Voltage get120Volts();
 }
