11e18017e555273093e17033d607c6c394366414
Slobodan Barisic <slbarisic@gmail.com>
2023-01-29 18:33:44 +0100

    added -env file and main class

---
+++ jwt/src/main/java/com/example/SpringSecurityInvestigationApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInvestigationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityInvestigationApplication.class, args);
+    }
+}
