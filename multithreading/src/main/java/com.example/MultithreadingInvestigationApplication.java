2f3977be38c2d05dad0e856fc704ff57dc9c23fa
Slobodan Barisic <slbarisic@gmail.com>
2023-01-29 09:20:57 +0100

    started with multithreading module

---
+++ multithreading/src/main/java/com.example/MultithreadingInvestigationApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingInvestigationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingInvestigationApplication.class, args);
+	}
+}
