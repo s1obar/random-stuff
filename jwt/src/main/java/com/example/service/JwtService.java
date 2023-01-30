d44f309bd5a3c986a4fd4f3a9ab46503c1a75925
Slobodan Barisic <slbarisic@gmail.com>
2023-01-30 20:59:37 +0100

    extracting claims

--- jwt/src/main/java/com/example/service/JwtService.java
+++ jwt/src/main/java/com/example/service/JwtService.java
@@ -1,10 +1,37 @@
 package com.example.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "4A614E645267556B58703273357638792F413F4428472B4B6250655368566D59";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T>T extractClaim(String token, Function<Claims, T> claimsResolver){
+       final Claims claims = extractAllClaims(token);
+       return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
