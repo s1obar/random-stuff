95e7e7ec7cc93b721bda95a6f804fb3101808d72
Slobodan Barisic <slbarisic@gmail.com>
2023-02-04 20:41:37 +0100

    started with angular and spring example

---
+++ angularspring/src/main/java/com/example/empman/AngularSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.empman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringApplication.class, args);
+	}
+
+}
---
+++ angularspring/src/main/java/com/example/empman/model/Employee.java
@@ -0,0 +1,11 @@
+package com.example.empman.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCOde
+}
